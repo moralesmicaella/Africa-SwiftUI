323fb0dc6d96400a7ccf4bdbf894bfee83e4009d
Micaella Morales <morales.micaella@gmail.com>
2022-02-16 18:40:55 -0800

    Initial Commit

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Micaella Morales on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+  var body: some Scene {
+    WindowGroup {
+      MainView()
+    }
+  }
+}
---
+++ Africa/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Micaella Morales on 2/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Content")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
