930c1eca3e7f002adf4e692ca3e7fc59ea9f3269
Micaella Morales <morales.micaella@gmail.com>
2022-02-18 22:44:26 -0800

    Add a grid layout view option in the content view

--- Africa/View/ContentView.swift
+++ Africa/View/ContentView.swift
@@ -9,26 +9,91 @@ import SwiftUI
 
 struct ContentView: View {
   // MARK: - PROPERTIES
+  @State private var isGridViewActive: Bool = false
+  @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+  @State private var gridIcon: String = "square.grid.2x2"
+  
   let animals: [Animal] = Bundle.main.decode("animals.json")
+  let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+  
+  // MARK: - FUNCTIONSS
+  func gridSwitch() {
+    gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+    
+    let gridColumns = gridLayout.count
+    switch(gridColumns) {
+    case 1:
+      gridIcon = "square.grid.2x2"
+    case 2:
+      gridIcon = "square.grid.3x2"
+    case 3:
+      gridIcon = "rectangle.grid.1x2"
+    default:
+      gridIcon = "square.grid.2x2"
+    }
+    
+  }
   
   // MARK: - BODY
   var body: some View {
     NavigationView {
-      List {
-        CoverImageView()
-          .frame(height: 300)
-          .listRowInsets(EdgeInsets())
-        
-        ForEach(animals) { animal in
-          NavigationLink(destination: AnimalDetailView(animal: animal)) {
-            AnimalListItemView(animal: animal)
-              .listRowBackground(Color.clear)
+      Group {
+        if !isGridViewActive {
+          List {
+            CoverImageView()
+              .frame(height: 300)
+              .listRowInsets(EdgeInsets())
+            
+            ForEach(animals) { animal in
+              NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                AnimalListItemView(animal: animal)
+                  .listRowBackground(Color.clear)
+              }
+            }
+          }
+          .listStyle(.plain)
+        } else {
+          ScrollView(.vertical, showsIndicators: false) {
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+              ForEach(animals) { animal in
+                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                  AnimalGridItemView(animal: animal)
+                }
+                .buttonStyle(.plain)
+              }
+            }
+            .padding(10)
+            .animation(.easeIn, value: isGridViewActive)
           }
         }
       }
-      .listStyle(.plain)
       .navigationTitle("Africa")
       .navigationBarTitleDisplayMode(.large)
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          HStack(spacing: 16) {
+            Button {
+              isGridViewActive = false
+              hapticImpact.impactOccurred()
+            } label: {
+              Image(systemName: "square.fill.text.grid.1x2")
+                .font(.title2)
+                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+            }
+            
+            Button {
+              isGridViewActive = true
+              gridSwitch()
+              hapticImpact.impactOccurred()
+            } label: {
+              Image(systemName: gridIcon)
+                .font(.title2)
+                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+            }
+          }
+          .buttonStyle(.plain)
+        }
+      }
     }
     .navigationViewStyle(.stack)
   }
